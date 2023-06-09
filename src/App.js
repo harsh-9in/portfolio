1e5964dcfc72a682106097356f58aea8481cf75f
harsh-9in <harshkumar9.in@gmail.com>
2023-06-09 22:19:13 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-
 import Footer from './Footer/Footer'
 import Main  from './Main/Main';
 import Contact from './Contact/Contact';
@@ -7,14 +6,27 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import NavbarHeader from './NavbarHeader/NavbarHeader';
 
+import HarshResume from './HarshResume.pdf';
+
 function App() {
+  const currentPage= ()=>{
+    switch(window.location.pathname){
+      case "/": return <Main/>;
+      case "/contact": return <Contact/>;
+      case "/resume": return <a href={`./HarshResume.pdf`} rel="noopener noreferrer" target="_blank">
+      PDF 
+   </a>;
+      default: return null;
+    }
+  }
+  
+
   return (
     <div>
       <header>
         <NavbarHeader/>
       </header>
-      <Main/>
-      {/* <Contact/> */}
+      {currentPage()}
       <Footer/>
       
     </div>
--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,27 +1,55 @@
 import './Contact.css';
 import '../index.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
-function Contact(){
-    return(
-            <div className="Contact">
-                <div className="Contact-Left">
-                    <span className='Contact-Left-element'>
-                        <span>Contact</span>
-                        <span>Looking forward to hearing form you</span>
-                    </span>
-                    <span className='Contact-Left-element'>
-                        <span>Phone</span>
-                        <span>8077184181</span>
-                    </span>
-                    <span className='Contact-Left-element'>
-                        <span>Email</span>
-                        <span>harshkumar9.in@gmail.com</span>
-                    </span>
-                </div>
-                <div className="Contact-Right">
-                    
-                </div>
-            </div>
+function Contact() {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <div className="Contact">
+                        <div className="Contact-Left">
+                            <span className='Contact-Left-element'>
+                                <span>Contact</span>
+                                <span>Looking forward to hearing form you</span>
+                            </span>
+                            <span className='Contact-Left-element'>
+                                <span>Phone</span>
+                                <span>8077184181</span>
+                            </span>
+                            <span className='Contact-Left-element'>
+                                <span>Email</span>
+                                <span>harshkumar9.in@gmail.com</span>
+                            </span>
+                        </div>
+                    </div>
+                </Col>
+                <Col>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="name" placeholder="Name" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="name@example.com" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Text</Form.Label>
+                            <Form.Control as="textarea" rows={3} />
+                        </Form.Group>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+
+        //     <div className="Contact-Right">
+
+        //     </div>
+        // </div>
     )
 }
 
